c39080180123402ea9a2b055cf449f5cad726b33
Steven Frasica <sfrasica1@gmail.com>
2020-01-02 15:43:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,5 +27,4 @@ end
 def get_english_meaning(file_path, emoticon)
   result = load_library(file_path)['get_meaning'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
-  # code goes here
 end
