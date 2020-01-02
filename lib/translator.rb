3ed5dd42582e86cebb06a50616fb6774e5922757
Steven Frasica <sfrasica1@gmail.com>
2020-01-02 15:43:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   result = load_library(file_path)['get_emoticon'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
-  # code goes here
+
 end
 
 def get_english_meaning(file_path, emoticon)
